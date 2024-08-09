5265e6e8cbca0ac8ff34f9653b56372a2e934926
Corey Jones <hackspark@outlook.com>
2024-08-09 14:30:29 -0400

    fixed

--- modules/screenshotish.py
+++ modules/screenshotish.py
@@ -6,10 +6,10 @@ import win32ui
 
 def get_dimensions():
     width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
     left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
     top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-    return (width, height, top, left)
+    return (width, height, left, top)
 
 def screenshot(name='screenshot'):
     hdesktop = win32gui.GetDesktopWindow()
@@ -19,20 +19,24 @@ def screenshot(name='screenshot'):
     img_dc = win32ui.CreateDCFromHandle(desktop_dc)
     mem_dc = img_dc.CreateCompatibleDC()
 
-    screenshot = win32ui.CreateBitmap()
-    screenshot.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot)
-    mem_dc.BitBlt((0,0), (width, height, img_dc, (left,top), win32con.SRCCOPYU))
-    screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp')
+    screenshot_bitmap = win32ui.CreateBitmap()
+    screenshot_bitmap.CreateCompatibleBitmap(img_dc, width, height)
+    mem_dc.SelectObject(screenshot_bitmap)
+
+    # Corrected attribute from SRCCOPYU to SRCCOPY
+    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+
+    screenshot_bitmap.SaveBitmapFile(mem_dc, f'{name}.bmp')
 
     mem_dc.DeleteDC()
-    wind32gui.DeleteObject(screenshot.GettHandle())
+    win32gui.DeleteObject(screenshot_bitmap.GetHandle())
 
-def run(): 
+def run():
     screenshot()
-    with open('screenshot.bmp') as f:
+    with open('screenshot.bmp', 'rb') as f:  # Changed to 'rb' mode for reading binary data
         img = f.read()
     return img
 
 if __name__ == '__main__':
     screenshot()
+
