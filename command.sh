bb08aa36b9020cf9a73c4d81415b4275c1281663
Corey Jones <hackspark@outlook.com>
2024-08-15 11:04:12 -0400

    added a way to use escape character to bash script

--- command.sh
+++ command.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+# Function to escape special characters in a string
+escape_special_chars() {
+  echo "$1" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g'
+}
+
 # Function to send the shell command
 send_shell_command() {
   echo "Enter the shell command you want to execute:"
   read shell_command
+  escaped_command=$(escape_special_chars "$shell_command")
   curl -X POST http://10.0.100.100:3000/send-command \
     -H "Content-Type: application/json" \
-    -d "{\"type\": \"shell\", \"command\": \"$shell_command\"}"
+    -d "{\"type\": \"shell\", \"command\": \"$escaped_command\"}"
 }
 
 # Function to send the module command
 send_module_command() {
   echo "Enter the module name you want to execute:"
   read module_name
+  escaped_module=$(escape_special_chars "$module_name")
   curl -X POST http://10.0.100.100:3000/send-command \
     -H "Content-Type: application/json" \
-    -d "{\"type\": \"module\", \"module\": \"$module_name\"}"
+    -d "{\"type\": \"module\", \"module\": \"$escaped_module\"}"
 }
 
 # Main script
@@ -30,5 +37,4 @@ elif [ "$choice" = "module" ]; then
 else
   echo "Invalid choice. Please run the script again and choose either 'shell' or 'module'."
 fi
-
 
