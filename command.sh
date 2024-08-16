a092103e2a335c622c2f5cb4f1707e3097ebf67e
Corey Jones <hackspark@outlook.com>
2024-08-16 16:24:08 -0400

    Added new module

--- command.sh
+++ command.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# Function to escape special characters in a string
+escape_special_chars() {
+  echo "$1" | sed 's/\\/\\\\/g' | sed 's/"/\\"/g'
+}
+
+# Function to send the shell command
+send_shell_command() {
+  echo "Enter the shell command you want to execute:"
+  read shell_command
+  escaped_command=$(escape_special_chars "$shell_command")
+  curl -X POST http://10.0.100.100:3000/send-command \
+    -H "Content-Type: application/json" \
+    -d "{\"type\": \"shell\", \"command\": \"$escaped_command\"}"
+}
+
+# Function to send the module command
+send_module_command() {
+  echo "Enter the module name you want to execute:"
+  read module_name
+  escaped_module=$(escape_special_chars "$module_name")
+  curl -X POST http://10.0.100.100:3000/send-command \
+    -H "Content-Type: application/json" \
+    -d "{\"type\": \"module\", \"module\": \"$escaped_module\"}"
+}
+
 # Function to send the C module command
 send_c_module_command() {
   echo "Enter the C module name you want to execute:"
